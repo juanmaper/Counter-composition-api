3af550e45ee6e30ce2fc133c817f44a34bb108f7
Juanma <juanmaper30@gmail.com>
2022-06-27 17:13:19 +0200

    Add basic logic for counter using composition api

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,10 +1,38 @@
 <template>
-  <h1>Counter view</h1>
+  <h1>CounterView</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
+
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/reactivity'
 
+export default {
+  name: 'Counter',
+  props: {},
+  emits: [],
+  setup() {
+
+    const counter = ref(5)
+
+    // const increase = () => {
+    //   counter.value++
+    // }
+
+    // const decrease = () => {
+    //   counter.value--
+    // }
+
+    return {
+      counter,
+
+      decrease: () => counter.value--,
+      increase: () => counter.value++
+    }
+  }
 }
 </script>
 
