031d150f8a336dc733c8a17cb5e589858d312718
Juanma <juanmaper30@gmail.com>
2022-07-04 14:02:45 +0200

    Design of view, state, add getters, use of them, computed properties, css classes to mark active button, add getter for getting current tab todos

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,32 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: [
+      { id: '1', text: 'Wash dishes', completed: false },
+      { id: '2', text: 'Clean room', completed: false },
+      { id: '3', text: 'Iron clothes', completed: true },
+      { id: '4', text: 'Shop food', completed: true },
+      { id: '5', text: 'Call my doctor', completed: false },
+    ]
   },
   getters: {
+    pendingTodos( state, getters, rootState ) {
+      return state.todos.filter( todo => !todo.completed )
+    },
+    allTodos: ( state ) => {
+      return state.todos
+    },
+    completedTodos: ( state ) => {
+      return state.todos.filter( todo => todo.completed )
+    },
+    getTodosByTab: ( _ , getters ) => ( tab ) => {
+
+      switch (tab) {
+        case 'all': getters.allTodos
+        case 'pending': getters.pendingTodos
+        case 'completed': getters.completedTodos
+      }
+    } 
   },
   mutations: {
   },
