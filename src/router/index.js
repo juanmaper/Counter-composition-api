766b87f7295eb332a09d4df4ac83fe9f8195da55
Juanma <juanmaper30@gmail.com>
2022-07-05 12:46:05 +0200

    Setup to use slot in component

--- src/router/index.js
+++ src/router/index.js
@@ -40,6 +40,11 @@ const routes = [
     name: 'todo',
     component: () => import(/* webpackChunkName: "TodoVuex" */ '../views/TodoVuex.vue')
   },
+  {
+    path: '/slots',
+    name: 'slots',
+    component: () => import(/* webpackChunkName: "Slots" */ '../views/CustomSlots.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/CustomSlots.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Learning Slots</h1>
+  <button>Open Modal</button>
+
+  <modal> 
+    <h2>Modal title</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa dignissimos vol</p>
+    <button>Exit</button>
+  </modal>
+</template>
+
+<script>
+import Modal from '../components/Modal'
+
+export default {
+  components: { Modal }
+}
+</script>
+
+<style>
+
+</style>
